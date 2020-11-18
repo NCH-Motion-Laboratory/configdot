728f05723890bcc43ef458ece47e0ce77addde57
Nurminen Jussi <jnu@iki.fi>
2020-11-18 15:48:58 +0200

    imports

--- configdot/__init__.py
+++ configdot/__init__.py
@@ -1,3 +1,11 @@
 import logging
 
+from .configdot import (
+    parse_config,
+    ConfigContainer,
+    ConfigItem,
+    update_config,
+    dump_config,
+)
+
 logging.getLogger('configdot').addHandler(logging.NullHandler())
